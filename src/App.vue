033b6a7ff718c58729f8b358e26c1c93fb8fd191
furecool <furecool@gmail.com>
2022-05-19 00:45:06 +0800

    coding style adjust

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,116 @@
 <template>
-  <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <!-- <v-app-bar
+        app
+        color="primary"
+        dark
+        >
+        <div class="d-flex align-center">
+            <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+            />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+            <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+            />
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
+        <v-btn
+            href="https://github.com/vuetifyjs/vuetify/releases/latest"
+            target="_blank"
+            text
+        >
+            <span class="mr-2">Latest Release</span>
+            <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+        </v-app-bar> -->
 
-    <div>
-      <button data-lang="tw" @click="setLang">中文</button>
-      <button data-lang="en" @click="setLang">English</button>
-    </div>
-
-    <div>
-      <div>{{ $t('GENERAL.FRIENDS_LIST') }}(3)</div>
-      <!-- <div>{{ $t('GENERAL.ITEMS') }}</div> -->
-      <!-- <div>{{ $t('GENERAL.MESSAGE') }}</div> -->
-      <!-- <div>{{ $t('GENERAL.ADD') }}</div> -->
-    </div>
-
-    <nav>
-      <div class="router">
-        <router-link to="/">
-          <div class="three-d-btn">Home</div>
-        </router-link>
-        <router-link to="/paul">
-          <div class="three-d-btn">保羅</div>
-        </router-link>
-        <router-link to="/jack">
-          <div class="three-d-btn">傑克</div>
-        </router-link>
-        <router-link to="/jason">
-          <div class="three-d-btn">傑森</div>
-        </router-link>
-      </div>
-    </nav>
-
-    <v-main>
-      <keep-alive>
-        <router-view/>
-      </keep-alive>
-    </v-main>
-  </v-app>
+        <div>
+            <button data-lang="tw" @click="setLang">中文</button>
+            <button data-lang="en" @click="setLang">English</button>
+        </div>
+        <div>
+            <div>{{ $t('GENERAL.FRIENDS_LIST') }}(3)</div>
+            <!-- <div>{{ $t('GENERAL.ITEMS') }}</div> -->
+            <!-- <div>{{ $t('GENERAL.MESSAGE') }}</div> -->
+            <!-- <div>{{ $t('GENERAL.ADD') }}</div> -->
+        </div>
+        <nav>
+            <div class="router">
+                <router-link to="/">
+                    <div class="three-d-btn">Home</div>
+                </router-link>
+                <router-link to="/paul">
+                    <div class="three-d-btn">保羅</div>
+                </router-link>
+                <router-link to="/jack">
+                    <div class="three-d-btn">傑克</div>
+                </router-link>
+                <router-link to="/jason">
+                    <div class="three-d-btn">傑森</div>
+                </router-link>
+            </div>
+        </nav>
+        <v-main>
+            <keep-alive>
+                <router-view/>
+            </keep-alive>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data: () => ({
-    data: []
-  }),
-  methods: {
-    setActiveLanguage(lang) {
-      localStorage.setItem('language', lang)
-    },
-    setLang(evt) {
-      const lang = evt.target.dataset.lang
-      this.setActiveLanguage(lang)
-      return history.go(0)
-    },
-  }
-};
+    name: 'App',
+    data: () => ({
+        data: []
+    }),
+    methods: {
+        setActiveLanguage(lang) {
+            localStorage.setItem('language', lang)
+        },
+        setLang(evt) {
+            const lang = evt.target.dataset.lang
+            this.setActiveLanguage(lang)
+            return history.go(0)
+        },
+    }
+}
 </script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- src/views/Paul.vue
+++ src/views/Paul.vue
@@ -1,123 +1,131 @@
 <template>
-  <div class="paul">
-
-    <h1>{{obj.name}}</h1>
-
-    <div>搜尋</div>    
-    <div>
-      <input v-model="keyWords" type="text" placeholder="請輸入關鍵字..." @keyup.enter="searchKey">{{ $t('GENERAL.ITEMS') }}
+    <div class="paul">
+        <h1>{{obj.name}}</h1>
+        <div>搜尋</div>    
+        <div>
+            <input
+                v-model="keyWords"
+                type="text"
+                placeholder="請輸入關鍵字..."
+                @keyup.enter="searchKey"
+            />
+            {{ $t('GENERAL.ITEMS') }}
+        </div>
+        <ul>
+            <li
+                v-for="(li, index) in obj.dialogue"
+                :key="index"
+                v-html="li"
+            ></li>
+        </ul>
+        <div @click="toogleMemo">備忘錄</div> 
+        <div v-if="memoDisplay">
+            <input v-model="newStr" :placeholder="$t('GENERAL.MESSAGE')" />
+            <button @click="addMemo">{{ $t('GENERAL.ADD') }}</button>
+            <div v-for="(item, index) in items" :key="index">
+                <p>{{item.date}} {{item.time}}</p>
+                <p>{{item.text}}</p>
+                <p @click="delMemo(index)">刪除</p>
+            </div>
+        </div>
     </div>
-    <ul>
-      <li v-for="(li, index) in obj.dialogue" :key="index" v-html="li"></li>
-    </ul>
-
-    <div @click="toogleMemo">備忘錄</div> 
-    <div v-if="memoDisplay">
-      <input v-model="newStr" :placeholder="$t('GENERAL.MESSAGE')"/>
-      <button @click="addMemo">{{ $t('GENERAL.ADD') }}</button>
-      <div v-for="(item, index) in items" :key="index">
-        <p>{{item.date}} {{item.time}}</p>
-        <p>{{item.text}}</p>
-        <p @click="delMemo(index)">刪除</p>
-      </div>
-    </div>
-
-  </div>
 </template>
-<script>
 
-  export default {
+<script>
+export default {
     name: 'PaulView',
     data() {
-      return {
-        keyWords: "",
-        results: [],
-        memoDisplay: false,
-        obj: {},
-        items: [
-          {
-            date:"2019/07/01",
-            time:"13:45",
-            text:"備忘訊息1",
-          },
-          {
-            date:"2019/07/01",
-            time:"13:45",
-            text:"備忘訊息2",
-          },
-        ],
-        newStr: ''
-      }
+        return {
+            keyWords: "",
+            results: [],
+            memoDisplay: false,
+            obj: {},
+            items: [
+                {
+                    date:"2019/07/01",
+                    time:"13:45",
+                    text:"備忘訊息1",
+                },
+                {
+                    date:"2019/07/01",
+                    time:"13:45",
+                    text:"備忘訊息2",
+                },
+            ],
+            newStr: ''
+        }
     },
     created() {
-      let vm = this
-      this.$ajax.get('/data.json')
-      .then(function(response) {
-        vm.data = response.data.data
-        vm.data.forEach(function(item){
-          if(item.name=="保羅"){
-            vm.obj = item
-          }
+        let vm = this
+        this.$ajax.get('/data.json')
+        .then(function(response) {
+            vm.data = response.data.data
+            vm.data.forEach(function(item) {
+                if (item.name=="保羅") {
+                    vm.obj = item
+                }
+            })
         })
-      })
-      .catch(function(err) { 
-          console.log(err)      
-      })      
+        .catch(function(err) { 
+            console.log(err)      
+        })      
     },
     methods: {
-      clearTimer () {
-        if (this.timer) {
-          clearTimeout(this.timer)
-        }
-      },
-      searchKey() {
-        let vm = this
-        this.clearTimer()
-        this.timer = setTimeout(() => {
-          this.$ajax.get('/data.json')
-          .then(function(response) {
-            vm.data = response.data.data
-            vm.data.forEach(function(item){
-              if(item.name=="保羅"){
-                vm.obj = item
-                vm.searchColor(vm.obj.dialogue)
-              }
+        clearTimer() {
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+        },
+        searchKey() {
+            let vm = this
+            this.clearTimer()
+            this.timer = setTimeout(() => {
+                this.$ajax.get('/data.json')
+                .then(function(response) {
+                    vm.data = response.data.data
+                    vm.data.forEach(function(item) {
+                        if (item.name=="保羅") {
+                            vm.obj = item
+                            vm.searchColor(vm.obj.dialogue)
+                        }
+                    })
+                })
+            }, 100)
+        },
+        searchColor(makeArr) {
+            let vm = this
+            makeArr.map(function(item, index) {
+                if (vm.keyWords && vm.keyWords.length > 0) {
+                    let replaceReg = new RegExp(vm.keyWords, 'g')
+                    let replaceString =
+                        '<span style="background: yellow;">' + vm.keyWords + '</span>'
+                    makeArr[index] = item.replace(
+                        replaceReg,
+                        replaceString
+                    )
+                }
             })
-          })
-        }, 100)
-      },
-      searchColor(makeArr) {
-        let vm = this
-        makeArr.map(function(item, index) {
-          if(vm.keyWords && vm.keyWords.length>0) {
-            let replaceReg = new RegExp(vm.keyWords, 'g')
-            let replaceString = '<span style="background: yellow;">' + vm.keyWords + '</span>'
-            makeArr[index] = item.replace(
-              replaceReg,
-              replaceString
+        },
+        toogleMemo() {
+            this.memoDisplay = !this.memoDisplay
+        },
+        addMemo() {
+            var Today=new Date()
+            this.items.push(
+                {
+                    date: Today.getFullYear() + "/" + (Today.getMonth()+1) + "/" + Today.getDate(),
+                    time: Today.getHours() + ":" + Today.getMinutes(),
+                    text: this.newStr,
+                },
             )
-          }
-        })
-      },
-      toogleMemo() {
-        this.memoDisplay = !this.memoDisplay
-      },
-      addMemo() {
-        var Today=new Date();
-        this.items.push(
-          {
-            date: Today.getFullYear() + "/" + (Today.getMonth()+1) + "/" + Today.getDate(),
-            time: Today.getHours() + ":" + Today.getMinutes(),
-            text: this.newStr,
-          },
-        );
-        this.newStr = '';
-      },
-      delMemo(index) {
-        this.items.splice(index, 1)
-      }
+            this.newStr = ''
+        },
+        delMemo(index) {
+            this.items.splice(index, 1)
+        }
     }
-  }
+}
 </script>
+
 <style lang="css" scoped>
 </style>
