82de45ca9e13cbbb05f36898b9956e1e156c7d32
furecool <furecool@gmail.com>
2022-05-19 15:45:11 +0800

    home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <h2>開始使用 Cherri Chat!</h2>
     <img src="../../public/img/img_default.png" alt="" height="130">
+    <h3>開始使用 Cherri Chat!</h3>
   </div>
 </template>
 
@@ -15,6 +15,8 @@
     },
   }
 </script>
-<style scoped>
-
+<style>
+h3 {
+  color: #4A90E2;
+}
 </style>
